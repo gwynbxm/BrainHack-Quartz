0967cf412425d970d07ee135b78e3306bb769326
Casuarina07 <casuarina070798@gmail.com>
2021-06-14 13:14:36 +0800

    Created components, added bottom navigation

---
+++ components/Register/Register.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Register() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Register!</Text>
+    </View>
+  );
+}
