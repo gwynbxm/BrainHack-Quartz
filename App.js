9cb8295f25340d980a735fe0f72edca23a84c4db
Sabby04 <54621243+Sabby04@users.noreply.github.com>
2021-06-14 21:12:07 +0800

    Raw flatlist and attraction details w no styles included

--- App.js
+++ App.js
@@ -1,45 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
-import Home from './components/Home/Home';
-import Profile from './components/Profile/Profile';
+import Home from "./components/Home/Home";
+import Profile from "./components/Profile/Profile";
+import AttractionDetail from "./components/Attraction/Attraction-detail";
+import AttractionList from "./components/Attraction/Attraction-list";
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator 
-        initialRouteName='Home'
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            if (route.name === "Others") {
-              iconName = "address-book";
-              focused ? (color = "teal") : (color = "gray");
-            } else if (route.name === "Home") {
-              iconName = "home";
-              focused ? (color = "teal") : (color = "gray");
-            } else if (route.name === "Profile") {
-              iconName = "user";
-              focused ? (color = "teal") : (color = "gray");
-            }
-
-            // You can return any component that you like here!
-            return <FontAwesome name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: 'teal',
-          inactiveTintColor: 'gray',
-        }}>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Profile" component={Profile} />
-      </Tab.Navigator>
-  </NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Attraction Places" component={AttractionList} />
+        <Stack.Screen name="Details" component={AttractionDetail} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
--- components/Attraction/Attraction-detail.js
+++ components/Attraction/Attraction-detail.js
@@ -1,10 +1,28 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import * as React from "react";
+import { Text, View, StyleSheet } from "react-native";
+/*
+  This js file will retrieve the details of attraction place from Attraction-list.js
+*/
+export default function AttractionDetail({ route }) {
+  const { Attraction, Description, Address, Coordinates, PhoneNo, Website } =
+    route.params;
 
-export default function AttractionDetail() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>AttractionDetail!</Text>
+    <View style={styles.container}>
+      <Text>Place: {Attraction}</Text>
+      <Text>Description: {Description}</Text>
+      <Text>Address: {Address}</Text>
+      <Text>Coordinates: {Coordinates}</Text>
+      <Text>Phone Number: {PhoneNo}</Text>
+      <Text>Website: {Website}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
