c16d291311fafa83cbff3bf44f34b71b985d8b2c
Gwyn Bong <gwynbong@gmail.com>
2021-06-15 15:04:43 +0800

    change tab icons, active and inactive color

--- App.js
+++ App.js
@@ -1,31 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
-import Home from './components/Home/Home';
-import Profile from './components/Profile/Profile';
+import Home from "./components/Home/Home";
+import Profile from "./components/Profile/Profile";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator 
-        initialRouteName='Home'
+      <Tab.Navigator
+        initialRouteName="Home"
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
             if (route.name === "Others") {
               iconName = "address-book";
-              focused ? (color = "teal") : (color = "gray");
+              focused ? (color = "navy") : (color = "skyblue");
             } else if (route.name === "Home") {
               iconName = "home";
-              focused ? (color = "teal") : (color = "gray");
+              focused ? (color = "navy") : (color = "skyblue");
             } else if (route.name === "Profile") {
               iconName = "user";
-              focused ? (color = "teal") : (color = "gray");
+              focused ? (color = "navy") : (color = "skyblue");
             }
 
             // You can return any component that you like here!
@@ -33,13 +33,13 @@ export default function App() {
           },
         })}
         tabBarOptions={{
-          activeTintColor: 'teal',
-          inactiveTintColor: 'gray',
-        }}>
+          activeTintColor: "navy",
+          inactiveTintColor: "skyblue",
+        }}
+      >
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Profile" component={Profile} />
       </Tab.Navigator>
-  </NavigationContainer>
+    </NavigationContainer>
   );
 }
-
